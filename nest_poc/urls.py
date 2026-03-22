a3e60edd198962fb024c35edc40c4a333d2018d3
Muhammad Hassaan Saleem <iamhassaans@gmail.com>
2026-03-22 22:16:36 +0500

    fix: remove useless endpoints

--- nest_poc/urls.py
+++ nest_poc/urls.py
@@ -3,13 +3,11 @@ from django.urls import path
 from api.views import (
     fast_ping,
     force_error,
-    hello_world,
     slow_request,
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/hello/', hello_world),
     path('api/ping/', fast_ping),
     path('api/slow/', slow_request),
     path('api/error/', force_error),
